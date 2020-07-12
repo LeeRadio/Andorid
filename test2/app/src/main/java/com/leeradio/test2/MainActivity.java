215fb68c725353359cd52731ef99fc9e084634e2
LeeRadio <acorn0987@naver.com>
2020-07-12 22:58:58 +0900

    화면 가로 고정 코드 추가
    화면 가로 고정코드를 추가하였다.

--- test2/app/src/main/java/com/leeradio/test2/MainActivity.java
+++ test2/app/src/main/java/com/leeradio/test2/MainActivity.java
@@ -2,6 +2,7 @@ package com.leeradio.test2;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -9,6 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE); //화면 가로 코드
         setContentView(R.layout.activity_main);
     }
 }
